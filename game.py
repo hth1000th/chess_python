647676b167bb2f1fa77cbb43507ad1fcf4327b2f
hth1000th <tahwang@puc.edu>
2016-03-16 12:15:00 -0700

    Display all chess pieces on the board.

--- game.py
+++ game.py
@@ -19,13 +19,49 @@ class Board(Widget):
     def __init__(self, **kwargs):
         super(Board, self).__init__(**kwargs)
         with self.canvas:
-            pass
+            width = [0,70,140,210,280,350,420,490]
+            height = [0,70,140,210,280,350,420,490]
+            num = 0
+            for y in height:
+                for x in width:
+                    if (num%2 == 0):
+                        Color(0,0,.5,mode='hsv')
+                        Rectangle(pos=(x,y), size=(70,70))
+                    else:
+                        Color(0,0,1,mode='rgb')
+                        Rectangle(pos=(x,y), size=(70,70))
+                    num += 1
+                num -= 1
 
 class Pieces(Board):
     def __init__(self, **kwargs):
         super(Pieces, self).__init__(**kwargs)
         with self.canvas:
-            pass
+            width = [0,70,140,210,280,350,420,490]
+
+            L_white_rook = Image(source="Images/white_rook.png", pos=(0, 490), size=(70,70))
+            L_white_knight = Image(source="Images/white_knight.png", pos=(70, 490), size=(70,70))
+            L_white_bishop = Image(source="Images/white_bishop.png", pos=(140, 490), size=(70,70))
+            white_king = Image(source="Images/white_king.png", pos=(210, 490), size=(70,70))
+            white_queen = Image(source="Images/white_queen.png", pos=(280, 490), size=(70,70))
+            R_white_bishop = Image(source="Images/white_bishop.png", pos=(350, 490), size=(70,70))
+            R_white_knight = Image(source="Images/white_knight.png", pos=(420, 490), size=(70,70))
+            R_white_rook = Image(source="Images/white_rook.png", pos=(490, 490), size=(70,70))
+            d={}
+            for x in width:
+                d["white_pawn" + str(x)] = Image(source="Images/white_pawn.png", pos=(x, 420), size=(70,70))
+
+            L_black_rook = Image(source="Images/black_rook.png", pos=(0, 0), size=(70,70))
+            L_black_knight = Image(source="Images/black_knight.png", pos=(70, 0), size=(70,70))
+            L_black_bishop = Image(source="Images/black_bishop.png", pos=(140, 0), size=(70,70))
+            black_king = Image(source="Images/black_king.png", pos=(210, 0), size=(70,70))
+            black_queen = Image(source="Images/black_queen.png", pos=(280, 0), size=(70,70))
+            R_black_bishop = Image(source="Images/black_bishop.png", pos=(350, 0), size=(70,70))
+            R_black_knight = Image(source="Images/black_knight.png", pos=(420, 0), size=(70,70))
+            R_black_rook = Image(source="Images/black_rook.png", pos=(490, 0), size=(70,70))
+            e={}
+            for x in width:
+                e["black_pawn" + str(x)] = Image(source="Images/black_pawn.png", pos=(x, 70), size=(70,70))
 
 class MyApp(App):
     def build(self):
