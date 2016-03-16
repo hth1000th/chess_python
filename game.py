95729a59b03aa432e707ac3273624d47deacf71e
hth1000th <tahwang@puc.edu>
2016-03-16 10:28:46 -0700

    proof of concept demonstrating basic graphical code

--- game.py
+++ game.py
@@ -7,9 +7,30 @@ from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
 from kivy.uix.image import Image
 
+class TouchInput(Widget):
+    def on_touch_down(self, touch):
+        print(touch)
+    def on_touch_move(self, touch):
+        print("on touch move")
+    def on_touch_up(self, touch):
+        print("RELEASE", touch)
+
+class Board(Widget):
+    def __init__(self, **kwargs):
+        super(Board, self).__init__(**kwargs)
+        with self.canvas:
+            pass
+
+class Pieces(Board):
+    def __init__(self, **kwargs):
+        super(Pieces, self).__init__(**kwargs)
+        with self.canvas:
+            pass
+
 class MyApp(App):
     def build(self):
-        pass
+        Window.size = (560, 560)
+        return Pieces()
 
 if __name__ == '__main__':
     MyApp().run()
